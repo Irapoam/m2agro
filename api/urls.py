acb39ff3a3e5371f94963f12ead814c495ae50e7
Irapoam Dias <irapoam.dias@gmail.com>
2016-11-22 02:18:33 -0200

    inicio do calculo

--- api/urls.py
+++ api/urls.py
@@ -2,14 +2,15 @@
 from django.conf.urls import include, url
 from rest_framework.routers import DefaultRouter
 
-from produtos.views import ProdutoViewSet
+from produtos.views import ProdutoViewSet, AtualizarProdutos
 from safras.views import SafraViewSet
 from servicos.views import ServicoViewSet, ServicoProdutoViewSet
 
 router = DefaultRouter()
 
 helper_patterns = [
-    url(r'^', include('rest_framework.urls', namespace='rest_framework')),    
+    url(r'^', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^atualizar-produtos/(?P<safra_id>[0-9]+)/$', AtualizarProdutos.as_view()),   
 ]
 
 router.register(r'produtos', ProdutoViewSet)
--- produtos/views.py
+++ produtos/views.py
@@ -1,9 +1,21 @@
+
 from .models import Produto
 from .serializer import ProdutoSerializer
+from safras.models import Safra
+from servicos.models import Servico, ServicoProduto
 
+
+from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 
 class ProdutoViewSet(ModelViewSet):
     queryset = Produto.objects.all()
     serializer_class = ProdutoSerializer
 
+class AtualizarProdutos(APIView):
+    
+    def get(self, request, safra_id):
+        safra = Safra.objects.get(id=safra_id)
+        servicos = Servico.objects.filter(safra=safra)
+        
+        
