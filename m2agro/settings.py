16bbe045b1c36094e7977ef2da5694ef2bbb0a71
Irapoam Dias <irapoam.dias@gmail.com>
2016-12-04 11:57:06 -0200

    static

--- m2agro/settings.py
+++ m2agro/settings.py
@@ -116,4 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static/')
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
